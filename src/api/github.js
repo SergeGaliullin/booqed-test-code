cf421986eb3acb10eb66021a52f9bae462697a13
sergei <sergei.galiullin@gmail.com>
2020-08-03 19:48:28 -0300

    Fix auth

--- src/api/github.js
+++ src/api/github.js
@@ -2,7 +2,4 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: 'https://api.github.com',
-    headers: {
-        Authorization: `Token 1dc5aa9e8d06c1ade2befc9e443f3de9532d4937`,
-    }
 });
